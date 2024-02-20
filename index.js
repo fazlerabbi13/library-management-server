ad3c3bfb202aa22a9f06f00e575c10102c88741c
fazlerabbi13 <mohammad0132794@gmail.com>
2024-02-21 03:01:56 +0600

    book update api create for update a book info on database

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
-
+require('dotenv').config()
 
 // midleware
 
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json())
 
 
@@ -25,6 +29,66 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    // DB Collections
+    const bookcategoryCollections = client.db('BookHub').collection('AllCategoryBooks');
+    const bookcategoryCollection = client.db('BookHub').collection('BookCategory');
+
+    // book category api
+
+    app.get('/bookcategory', async (req, res) => {
+      const cursor = bookcategoryCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    // addbooks api
+    app.post('/addbooks', async (req, res) => {
+      const newBook = req.body;
+      const result = await bookcategoryCollections.insertOne(newBook);
+      res.send(result);
+    })
+    //  added all books for geeting api
+    app.get('/addedbooks', async (req, res) => {
+      const cursor = bookcategoryCollections.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    // update book api
+    app.get('/addedbooks/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookcategoryCollections.findOne(query);
+      res.send(result);
+    })
+
+    app.put('/addedbooks/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateBook = req.body
+      const product = {
+        $set: {
+          image: updateBook.image,
+          bookName: updateBook.bookName,
+          quantityBook: updateBook.quantityBook,
+          authorName: updateBook.authorName,
+          category: updateBook.category,
+          short: updateBook.short,
+          rating: updateBook.rating
+        }
+      }
+      const result = await bookcategoryCollections.updateOne(filter, product, options)
+      res.send(result)
+    })
+    // testing the server
+    app.get('/', (req, res) => {
+      res.send('car doctor is running')
+    })
+
+
+    app.listen(port, () => {
+      console.log(`car doctor is running on port ${port}`)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
