5c032087a566b2afadff91e419a4c4a13cd141a9
fazlerabbi13 <mohammad0132794@gmail.com>
2024-02-26 17:33:30 +0600

    add jwt token for sequrity and verfy the user

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -11,7 +13,8 @@ app.use(cors({
   origin: ['http://localhost:5173'],
   credentials: true
 }));
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nruv7rx.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,6 +28,39 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+// const verifyToken = (req, res, next) => {
+//   const token = req?.cookies?.token;
+//   console.log(token)
+//   // console.log('token in the middleware', token);
+//   // no token available 
+//   if (!token) {
+//     return res.status(401).send({ message: 'unauthorized access' })
+//   }
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).send({ message: 'unauthorized access' })
+//     }
+//     req.user = decoded;
+//     next();
+//   })
+// }
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -33,6 +69,53 @@ async function run() {
     const bookcategoryCollections = client.db('BookHub').collection('AllCategoryBooks');
     const bookcategoryCollection = client.db('BookHub').collection('BookCategory');
     const borrowedBooks = client.db('BookHub').collection('BorrowedBooks');
+
+
+
+    // auth related api
+    // app.post('/jwt', async (req, res) => {
+    //   const user = req.body;
+    //   console.log('user for token', user);
+    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+    //   console.log(token)
+    //   res.cookie('token', token, {
+    //     httpOnly: true,
+    //     secure: true,
+    //     sameSite: 'none'
+    //   })
+    //     .send({ success: true });
+    // })
+
+    // app.post('/logout', async (req, res) => {
+    //   const user = req.body;
+    //   console.log('logging out', user);
+    //   res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+    // })
+
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.ACCESS_TOKEN_SECRET === "production",
+          sameSite: process.env.ACCESS_TOKEN_SECRET === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
+    app.post("/logout", async (req, res) => {
+      res
+        .clearCookie("token", {
+          maxAge: 0,
+          secure: process.env.ACCESS_TOKEN_SECRET === "production" ? true : false,
+          sameSite: process.env.ACCESS_TOKEN_SECRET === "production" ? "none" : "strict",
+        })
+        .send({ status: true });
+    });
+
 
     // book category api
 
@@ -49,15 +132,26 @@ async function run() {
       res.send(result);
     })
     //  borrowed book api
-    app.post('/borrowedbooks', async(req, res) =>{
+    app.post('/borrowedbooks', async (req, res) => {
+
       const borrowedBook = req.body;
       const result = await borrowedBooks.insertOne(borrowedBook);
       res.send(result)
     })
 
-    app.get('/borrowedbooks', async (req, res) => {
-      const cursor = borrowedBooks.find();
-      const result = await cursor.toArray()
+    app.get('/borrowedbooks', verifyToken, async (req, res) => {
+       console.log(req.query.email);
+       console.log(req.user.email);
+      const queryEmail = req?.query?.email;
+
+      if (req?.user?.email !== queryEmail) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      let query = {};
+      if (queryEmail) {
+        query = { userEmail: queryEmail }
+      }
+     const result=await borrowedBooks.find(query).toArray();
       res.send(result)
     })
     // getting all data and paigination api
